8335aad4cba237edd8218e1fce7813f7a0e337b0
Krunal Patel <krunalpintoo@gmail.com>
2018-01-22 22:13:42 -0500

    adding catering page

--- src/app/services/food-catering/food-order/food-order.component.ts
+++ src/app/services/food-catering/food-order/food-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-food-order',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-order.component.scss']
 })
 export class FoodOrderComponent implements OnInit {
-
+  cateringOrderDetail: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.cateringOrderDetail =  new FormGroup({
+      'firstName': new FormControl(null, [Validators.required, Validators.pattern('^([A-Za-z]+[,.]?[ ]?|[A-Za-z]+[\'-]?)+$')]),
+      'lastName': new FormControl(null, [Validators.required, Validators.pattern('^([A-Za-z]+[,.]?[ ]?|[A-Za-z]+[\'-]?)+$')]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'phone': new FormControl(null, [Validators.required, Validators.pattern('^\d{10}$')]),
+      'address': new FormGroup({
+        'addressLine1': new FormControl(null , Validators.required),
+        'addressLine2': new FormControl(null),
+        'city': new FormControl(null , Validators.required),
+        'state': new FormControl(null , Validators.required),
+        'zipCode': new FormControl(null , [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')]),
+      })
+    });
   }
 
 }
